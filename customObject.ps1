050d8e5ca320957b20e76cd4eac228aa66f039d4
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2025-07-17 17:52:11 +0530

    Update customObject.ps1

--- customObject.ps1
+++ customObject.ps1
@@ -1,5 +1,5 @@
-$person= [PSCustomObject]@{
-    FirstName = "Ravi"
+$person= [PSCustomObject]@{                # person is Custom Object
+    FirstName = "Ravi"                     # FirstName, LastName, etc are Properties of the object Person
     LastName = "Ranjan"
     Age = 32
     Occupaation = "DevOps Engineer"
