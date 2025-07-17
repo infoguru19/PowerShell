7fc1f468b4c184c235b999ff64701d4056fa9a3c
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2025-07-17 17:42:39 +0530

    Update PSTest1.ps1

--- PSTest1.ps1
+++ PSTest1.ps1
@@ -4,3 +4,7 @@ Get-Command                 # List all the commands
 Get-Command -Noun Service   # List commands associated with the Service noun
 Get-Command -Verb Install   # List Commands all Commands with Install Verb
 
+# Alias:
+# Alias of Get-Service is gsv
+gsv #Same Result as Get-Service
+
