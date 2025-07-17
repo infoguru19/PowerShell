98989a2306696a7a81ce424a7c34e5a672da0a14
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2025-07-17 17:54:30 +0530

    Update pipeline.ps1

--- pipeline.ps1
+++ pipeline.ps1
@@ -2,6 +2,7 @@
 # Command1 | Command2 | Command3
 
 "Hello World" | ForEach-Object{$_.ToUpper()}
+
 Get-Process | Where-Object{$_.Name -eq "Wininit"} | Select-Object Id, Name
 Get-Service | Where-Object{$_.Status -eq "Running"}
 Get-ChildItem -Path "C:\Users\ravic\Downloads" | Where-Object{$_.Length -gt 100MB}
